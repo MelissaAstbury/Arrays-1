8fe84324bef939f9fbaf4ae806a97d430b6178ea
Melissa Astbury <melissajastbury@gmail.com>
2020-02-02 20:35:03 +0000

    Practicing arrays

---
+++ arrays.js
@@ -0,0 +1,34 @@
+//EXPERCISE ONE
+
+var numbers = [22, 67, 33, 96, 88];
+//console.log(numbers[numbers.length]);
+//console.log(numbers.length);
+
+//EXPERCISE TWO
+// Log out the whole array
+
+//console.log(numbers);
+
+//Part Two
+// Console.log 96 from the array above
+
+//console.log(numbers[3]);
+
+//EXPERCISE THREE
+// Store 88 from the array into a new variable called specialNum
+
+let specialNum = 4;
+console.log(specialNum);
+//console.log(typeof specialNum);
+// if (specialNum !== "88") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+//console.log(specialNum);
+specialNum = numbers[4];
+console.log(specialNum);
+
+specialNum = numbers[2];
+console.log(specialNum);
